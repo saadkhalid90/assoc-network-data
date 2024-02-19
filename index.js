e8a520fbe29b3f20118da7a1c519247841d5d046
Saad Khalid <skhalid@secdev.local>
2024-02-19 09:13:42 -0800

    duplication removed from indexjs

--- index.js
+++ index.js
@@ -1,112 +1,185 @@
 import { dummyAssocNetwork } from "./dummyNetwork.js";
 
-function getNthDegreeConns(rootID, nDegree, linksMap, assoc = true) { 
-  const associates = assoc ? new Set([rootID]) : new Set();
-  const entities = assoc ? new Set() : new Set([rootID]);
-  const leafEntities = new Set();
-  const leafEntitesConn = {};
-  const links = new Set();
-  function commulConn(rootID, nDegree, linksMap, currentDegree = 1, assoc) {
-    const currentConn = linksMap[rootID] ? linksMap[rootID] : [];
-    currentConn.forEach((conn) => {
-      const nodeExists = assoc
-        ? entities.has(conn) || leafEntities.has(conn)
-        : associates.has(conn);
-      if (!nodeExists) {
-        if (assoc) {
-          nDegree !== currentDegree
-            ? entities.add(conn)
-            : leafEntities.add(conn);
-        } else {
-          associates.add(conn);
-        }
-      }
-      const linkId = assoc ? `${rootID}-${conn}` : `${conn}-${rootID}`;
-      const linkExists = links.has(linkId);
-      if (!linkExists) {
-        links.add(linkId);
-        if (nDegree === currentDegree) {
-          if (!leafEntitesConn[conn]) {
-            leafEntitesConn[conn] = 1;
+class AssociateNetwork {
+  constructor(rootID, assoc, linksMap) {
+    const associates = assoc ? new Set([rootID]) : new Set();
+    const entities = assoc ? new Set() : new Set([rootID]);
+    const leafEntities = new Set();
+    const leafEntitesConn = {};
+    const links = new Set();
+
+    Object.assign(this, {
+      associates,
+      entities,
+      leafEntities,
+      leafEntitesConn,
+      links,
+      assoc,
+      rootID,
+      linksMap,
+    });
+  }
+
+  getInitialNetwork() {
+    const {
+      associates,
+      entities,
+      leafEntities,
+      leafEntitesConn,
+      links,
+      rootID,
+      linksMap,
+    } = this;
+    let { assoc } = this;
+    const nDegree = assoc ? 3 : 2;
+    function depthFirstTrav(
+      rootID,
+      nDegree,
+      linksMap,
+      currentDegree = 1,
+      assoc
+    ) {
+      const currentConn = linksMap[rootID] ? linksMap[rootID] : [];
+      currentConn.forEach((conn) => {
+        const nodeExists = assoc
+          ? entities.has(conn) || leafEntities.has(conn)
+          : associates.has(conn);
+        if (!nodeExists) {
+          if (assoc) {
+            nDegree !== currentDegree
+              ? entities.add(conn)
+              : leafEntities.add(conn);
           } else {
-            leafEntitesConn[conn]++;
+            associates.add(conn);
           }
         }
-      }
-    });
-    assoc = !assoc; // boolean - whether our layer is associates (or entities)
-    currentDegree++;
-    if (currentDegree <= nDegree) {
-      currentConn.forEach((nodeId) =>
-        commulConn(nodeId, nDegree, linksMap, currentDegree, assoc)
-      );
-    }
-  }
-  commulConn(rootID, nDegree, linksMap, 1, assoc);
-  return {
-    associates,
-    entities,
-    leafEntities,
-    leafEntitesConn,
-    links,
-  };
-}
-
-function getUnexploredAssociates(assocId, linksMap, currentNetwork) {
-  const linkedEntites = linksMap[assocId];
-  const unexpAssocs = new Set();
-  linkedEntites.forEach((entityId) => {
-    if (currentNetwork.leafEntities.has(entityId)) {
-      const linkedAssocs = linksMap[entityId];
-      linkedAssocs.forEach((assoc) => {
-        if (!currentNetwork.associates.has(assoc)) {
-          unexpAssocs.add(assoc);
+        const linkId = assoc ? `${rootID}-${conn}` : `${conn}-${rootID}`;
+        const linkExists = links.has(linkId);
+        if (!linkExists) {
+          links.add(linkId);
+          if (nDegree === currentDegree) {
+            if (!leafEntitesConn[conn]) {
+              leafEntitesConn[conn] = 1;
+            } else {
+              leafEntitesConn[conn]++;
+            }
+          }
         }
       });
-    }
-  });
-  return unexpAssocs;
-}
-
-function expandAssoc(assocId, linksMap, currentNetwork) {
-  const unexpAssocs = Array.from(
-    getUnexploredAssociates(assocId, linksMap, currentNetwork)
-  );
-
-  unexpAssocs.forEach((assoc) => {
-    const linkedEntites = linksMap[assoc];
-    currentNetwork.associates.add(assoc);
-    linkedEntites.forEach((entity) => {
-      if (currentNetwork.leafEntities.has(entity)) {
-        currentNetwork.leafEntitesConn[entity]++;
-        if (
-          linksMap[entity].length === currentNetwork.leafEntitesConn[entity]
-        ) {
-          currentNetwork.entities.add(entity);
-          currentNetwork.leafEntities.delete(entity);
-        }
-      } else if (
-        !currentNetwork.leafEntities.has(entity) &&
-        !currentNetwork.entities.has(entity)
-      ) {
-        currentNetwork.leafEntities.add(entity);
-        currentNetwork.leafEntitesConn[entity] = 1;
+      assoc = !assoc; // boolean - whether our layer is associates (or entities)
+      currentDegree++;
+      if (currentDegree <= nDegree) {
+        currentConn.forEach((nodeId) =>
+          depthFirstTrav(nodeId, nDegree, linksMap, currentDegree, assoc)
+        );
       }
+    }
+    depthFirstTrav(rootID, nDegree, linksMap, 1, assoc);
+    return this;
+  }
 
-      currentNetwork.links.add(`${assoc}-${entity}`);
+  getUnexpAssocs(assocId) {
+    const { linksMap } = this;
+    const linkedEntites = linksMap[assocId];
+    const unexpAssocs = new Set();
+    linkedEntites.forEach((entityId) => {
+      if (this.leafEntities.has(entityId)) {
+        const linkedAssocs = linksMap[entityId];
+        linkedAssocs.forEach((assoc) => {
+          if (!this.associates.has(assoc)) {
+            unexpAssocs.add(assoc);
+          }
+        });
+      }
     });
-  });
+    return unexpAssocs;
+  }
 
-  return currentNetwork;
+  expandAssoc(assocId) {
+    const unexpAssocs = Array.from(this.getUnexpAssocs(assocId));
+    const { linksMap } = this;
+
+    unexpAssocs.forEach((assoc) => {
+      const linkedEntites = linksMap[assoc];
+      this.associates.add(assoc);
+      linkedEntites.forEach((entity) => {
+        if (this.leafEntities.has(entity)) {
+          this.leafEntitesConn[entity]++;
+          if (linksMap[entity].length === this.leafEntitesConn[entity]) {
+            this.entities.add(entity);
+            this.leafEntities.delete(entity);
+          }
+        } else if (
+          !this.leafEntities.has(entity) &&
+          !this.entities.has(entity)
+        ) {
+          this.leafEntities.add(entity);
+          this.leafEntitesConn[entity] = 1;
+        }
+
+        this.links.add(`${assoc}-${entity}`);
+      });
+    });
+
+    return this;
+  }
+
+  getGroupedNetwork() {
+    const entityGrps = {};
+    const links = Array.from(this.links);
+
+    links.forEach((link) => {
+      const [assocId, entId] = link.split("-");
+      if (!entityGrps[entId]) {
+        entityGrps[entId] = [];
+      }
+      entityGrps[entId].push(assocId);
+    });
+
+    for (const [entityId, groupIds] of Object.entries(entityGrps)) {
+      entityGrps[entityId] = groupIds.sort().join(",");
+    }
+
+    const linksArray = links.map((link) => {
+      const [assocId, entId] = link.split("-");
+      return {
+        assoc: assocId,
+        entity: entId,
+        entityGrp: entityGrps[entId],
+      };
+    });
+
+    const linksGrp = {};
+
+    // nesting links
+    linksArray.forEach((link) => {
+      const { entityGrp, assoc } = link;
+
+      const linkId = `${assoc}|${entityGrp}`;
+      if (!linksGrp[linkId]) {
+        linksGrp[linkId] = {
+          assoc,
+          entityGrp,
+          sublinks: [],
+        };
+      }
+      linksGrp[linkId].sublinks.push(link);
+    });
+
+    const linksGrpArr = Object.values(linksGrp);
+
+    const listEntities = (linkGrp) => {
+      const entitiesArr = linkGrp.sublinks.map((d) => d.entity);
+      return entitiesArr.join(",");
+    };
+
+    return linksGrpArr.map((d) => ({
+      ...d,
+      entitiesList: listEntities(d),
+    }));
+  }
 }
 
-const myNetwork = getNthDegreeConns("Alex", 3, dummyAssocNetwork, true);
-console.log(myNetwork.associates);
-console.log(myNetwork);
-
-// console.log(getUnexploredAssociates("Meherbano", dummyAssocNetwork, myNetwork));
-// console.log(expandAssoc("Meherbano", dummyAssocNetwork, myNetwork));
-// console.log(getUnexploredAssociates("Saleem", dummyAssocNetwork, myNetwork));
-// console.log(expandAssoc("Saleem", dummyAssocNetwork, myNetwork));
-// console.log(getUnexploredAssociates("John", dummyAssocNetwork, myNetwork));
-// console.log(expandAssoc("John", dummyAssocNetwork, myNetwork));
+const network = new AssociateNetwork("Alex", true, dummyAssocNetwork);
+network.getInitialNetwork();
+console.log(network.getGroupedNetwork());
